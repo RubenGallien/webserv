0231f7db252d98c51aaa9be04ef8c7d6ad3865c6
Ruben Gallien <rgallien@f4r6s9.clusters.42paris.fr>
2025-07-02 16:28:15 +0200

    init and try socket

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ int main()
     int socketFD;
     const int port = 8888;
     const std::string addrIp = "127.0.0.1";
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
     socketFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (socketFD == -1)
         std::cout << strerror(errno) << std::endl;
+
+    if (bind(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
+        std::cout << strerror(errno) << std::endl;
+
+    
     if (listen(socketFD, 1024) == -1)
          std::cout << strerror(errno) << std::endl;
-    
-    
-    close(socketFD);
 
+    close(socketFD);
+    return (0);
 }
