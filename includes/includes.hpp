ea7980c4a4eee2c800366d26e8bfa6545b82f1fe
RubenGallien <ruben-gallienpro@outlook.fr>
2025-07-06 22:26:40 +0000

    files

--- includes/includes.hpp
+++ includes/includes.hpp
@@ -6,3 +6,5 @@
 #include <string.h>
 #include <cstdlib>
 #include <stdio.h>
+#include <fstream>
+#include <sstream>
--- main.cpp
+++ main.cpp
@@ -10,19 +10,30 @@ int main()
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(port);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
+    
+    std::ifstream src("./files/default.html");
+    std::string dest;
+    std::string line;
+
+    if (src.is_open())
+    {
+        while (getline(src, line))
+            dest += line + '\n';
+    }
+
 
     socklen_t serverAddressSize = sizeof(serverAddress);
 
     socketFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (socketFD == -1)
-        std::cout << strerror(errno) << " 18 " << std::endl;
+        std::cout << strerror(errno) << std::endl;
 
     if (bind(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
-        std::cout << strerror(errno) << " 21 " <<  std::endl;
+        std::cout << strerror(errno) <<  std::endl;
 
     
     if (listen(socketFD, 1024) == -1)
-         std::cout << strerror(errno) << " 25 " << std::endl;
+         std::cout << strerror(errno) << std::endl;
 
 
     while(1)
@@ -30,9 +41,21 @@ int main()
         memset(buffer, 0, sizeof(buffer));
         socketAccept = accept(socketFD, (struct sockaddr*)&serverAddress, &serverAddressSize);
         if (socketAccept == -1)
-            std::cout << strerror(errno) << " 33 " << std::endl;
-        recv(socketAccept, buffer, 1024, 0);
-        std::cout << buffer << std::endl;
+            std::cout << strerror(errno) <<  std::endl;
+        if (recv(socketAccept, buffer, 1024, 0))
+        {
+            std::string result;
+            std::ostringstream convert;
+            convert << dest.size();
+            result = convert.str();
+            std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: " + result + "\r\n"
+            "Connection: close\r\n"
+            "\r\n" + dest;
+            send(socketAccept, response.c_str(), response.size(), 0);
+        }
         
     }
 
